e846f31dbc71972944908aed8a3746b1d4bdbf8f
William Dreese <wdreese123@gmail.com>
2022-02-19 14:56:24 -0500

    updated naming, can add all col types

---
+++ client/pycli.go
@@ -0,0 +1,58 @@
+/*
+    CPython interface to cache
+*/
+package main
+
+import (
+    "C"
+//    "fmt"
+)
+
+var StorePtr *Store = nil
+
+//export Initstore
+func Initstore() bool {
+    if StorePtr != nil {
+        return false
+    }
+    StorePtr = NewStore()
+    return true
+}
+
+//export Newcoll
+func Newcoll(name *C.char) bool {
+    if StorePtr == nil {
+        return false
+    }
+    nameStr := C.GoString(name)
+    if err := StorePtr.NewCollection(nameStr); err != nil {
+        return false
+    }
+    return true
+}
+
+//export AddCol
+func AddCol(collection, colname, coltype *C.char) bool {
+    if StorePtr == nil {
+        return false
+    }
+    cl := C.GoString(collection)
+    cn := C.GoString(colname)
+    ct := C.GoString(coltype)
+    if err := StorePtr.AddColumn(cl, cn, ct); err != nil {
+        return false
+    }
+    return true
+}
+
+//export Set
+func Set(coll string) bool {
+    return false
+}
+
+//export Get
+func Get(coll string) bool {
+    return false
+}
+
+func main() {}
--- client/tester.py
+++ client/tester.py
@@ -1,18 +1,39 @@
+import sys, os
 from ctypes import *
 
-def main():
-    pystore = cdll.LoadLibrary('pydc_client.so')
-    pystore.AddCol.argtypes = [c_char_p, c_char_p, c_char_p]
+default_path = 'bin/'
+default_fn   = 'pydc_client.so' 
 
-    print(pystore.Initstore())
-    print(pystore.Newcoll("testcoll".encode('utf-8')))
-    print(pystore.AddCol(
+def main(args):
+    lib_path = os.path.join(args[1] if len(args) > 1 else default_path, default_fn) 
+    pydc = cdll.LoadLibrary(lib_path)
+
+    print(pydc.Initstore())
+    print(pydc.Newcoll("testcoll".encode('utf-8')))
+    print(pydc.AddCol(
         "testcoll".encode('utf-8'),
         "testcol".encode('utf-8'), 
         "int".encode('utf-8'),
     ))
+    print(pydc.AddCol(
+        "testcoll".encode('utf-8'),
+        "testcolstr".encode('utf-8'), 
+        "string".encode('utf-8'),
+    ))
+    print(pydc.AddCol(
+        "testcoll".encode('utf-8'),
+        "testcolflo".encode('utf-8'), 
+        "float".encode('utf-8'),
+    ))
+    print(pydc.AddCol(
+        "testcoll".encode('utf-8'),
+        "testcolbool".encode('utf-8'), 
+        "bool".encode('utf-8'),
+    ))
+    
+
     print('done')
 
 
 if __name__=="__main__":
-    main() 
+    main(sys.argv) 
