6f51feabc2ca2260fe37c3057508dfc31e5bd8d2
William Dreese <wdreese123@gmail.com>
2022-03-12 14:16:28 -0500

    addobject test passing

--- client/storage_test.go
+++ client/storage_test.go
@@ -6,22 +6,20 @@ package main
 import (
     "testing"
 
-    // "github.com/stretchr/testify/assert"
+    "github.com/kelindar/column"
+    "github.com/stretchr/testify/assert"
 )
 
 func Test_AddColumn(t *testing.T) {
 
     test_store := NewStore()
-
     test_store.NewCollection("test_coll")
     
-
     expectedNil := test_store.AddColumn("test_coll", "testcolumn", "int")
     if expectedNil != nil {
-        t.Errorf("Column type error expected, %v returned", expectedNil.Error())
+        t.Errorf("Nil expected, %v returned", expectedNil.Error())
     }
 
-    
     expectedErr := test_store.AddColumn("test_coll", "testcolumn_fail", "faketype")
     if expectedErr.Error() != "Column type does not exist" {
         t.Errorf("Column type error expected, %v returned", expectedErr.Error())
@@ -32,11 +30,31 @@ func Test_AddColumn(t *testing.T) {
         t.Errorf("Column name already exists expected, %v returned", expectedErr.Error())
     }
 
+    cmm, _ := test_store.CollMetadataMap["test_coll"]
+    assert.Equal(t, cmm[0].Name, "testcolumn")
+    assert.Equal(t, cmm[0].Type, "int")
 }
 
-//func Test_AddObject(t *testing.T) {
+func Test_AddObject(t *testing.T) {
+    test_store := NewStore()
+    test_store.NewCollection("test_coll")
+    
+    _ = test_store.AddColumn("test_coll", "testcolumnint", "int")
+    _ = test_store.AddColumn("test_coll", "testcolumnstr", "string")
+    _ = test_store.AddColumn("test_coll", "testcolumnfloat", "float")
+    _ = test_store.AddColumn("test_coll", "testcolumnbool", "bool")
+    
+    test_obj := []interface{}{16,"tester",73.8,false}
+    test_store.AddObject("test_coll", test_obj)
 
-//}
+    coll, _ := test_store.CollMap["test_coll"]
+    var count int
+    coll.Query(func (txn *column.Txn) error {
+        count = txn.Count()
+        return nil
+    })
+    assert.Equal(t, count, 1)
+}
 
 //func Test_Select(t *testing.T) {
 
