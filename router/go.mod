88fd9fee75ad0d346a973651223bf095794357ca
William Dreese <wdreese123@gmail.com>
2022-03-20 12:19:12 -0400

    read yaml of tables

--- router/go.mod
+++ router/go.mod
@@ -2,7 +2,10 @@ module github.com/Dreeseaw/salmon/router
 
 go 1.17
 
-require github.com/labstack/echo/v4 v4.7.2
+require (
+	github.com/labstack/echo/v4 v4.7.2
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
--- router/main.go
+++ router/main.go
@@ -1,20 +1,75 @@
 package main
 
 import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "gopkg.in/yaml.v3"
+
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo/v4/middleware"
-    "net/http"
 )
 
+type Server struct {
+    Tables map[string]Table
+}
+
+func NewServer() *Server {
+    return &Server{
+        Tables: make(map[string]Table),
+    }
+}
+
+func (s *Server) ReadConfig(filePath string) {
+    yfile, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        panic(err)
+    }
+
+    data := make(map[interface{}]interface{})
+
+    err = yaml.Unmarshal(yfile, &data)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(data)
+
+    for tName, tCols := range data {
+        cols := make(map[string]column)
+        for colName, colData := range tCols.(map[string]interface{}) {
+            newCol := column{
+                Type: (colData.(map[string]interface{}))["type"].(string),
+            }
+            cols[colName] = newCol
+        }
+        s.Tables[tName.(string)] = Table{Cols: cols}
+    }
+
+    fmt.Println(s.Tables)
+}
+
+// TODO: add more metadata
+type column struct {
+    Type string
+}
+
+type Table struct {
+    Cols map[string]column
+}
+
 func main() {
+    s := NewServer()
+    s.ReadConfig("tester.yaml")
+
     e := echo.New()
 
     e.Use(middleware.Logger())
-    e.GET("/", hello)
+    e.GET("/accept", s.accept)
 
     e.Logger.Fatal(e.Start(":1323"))
 }
 
-func hello(c echo.Context) error {
+func (s *Server) accept(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
 }
