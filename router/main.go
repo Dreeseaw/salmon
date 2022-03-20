618d9f69a8624b118c5ac3dbdb00e2219896beb1
William Dreese <wdreese123@gmail.com>
2022-03-19 20:34:13 -0400

    init package

---
+++ router/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
+    "net/http"
+)
+
+func main() {
+    e := echo.New()
+
+    e.Use(middleware.Logger())
+    e.GET("/", hello)
+
+    e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, World!")
+}
