91a55c2dcfd2a840d97ec0b4562adf214722ba67
William Dreese <wdreese123@gmail.com>
2022-04-03 17:28:29 -0400

    example working now (#9)
    why was this actually difficult?

--- examples/example.go
+++ examples/example.go
@@ -1,25 +1,29 @@
 /*
-    A very simple usage example
+   A very simple usage example
 
-    Requires a running Salmon Router @ localhost:27604
+   Requires a running Salmon Router @ localhost:27604
 */
 package main
 
 import (
-    salmon "github.com/Dreeseaw/salmon/client"
+	"fmt"
+    "time"
+	"os"
+
+	salmon "github.com/Dreeseaw/salmon/client"
 )
 
 const (
-    configFile string = "/tmp/salmon.yaml"
+	configFile string = "/tmp/salmon.yaml"
 )
 
 func write_config() {
 
-    if _, err := os.Stat(configFile); err == nil {
-        os.Remove(configFile)
-    }
+	if _, err := os.Stat(configFile); err == nil {
+		os.Remove(configFile)
+	}
 
-    yaml_txt := []byte(`testtable:
+	yaml_txt := []byte(`testtable:
   testcolumnint:
     type: int
     name: testcolumnint
@@ -36,21 +40,34 @@ func write_config() {
     type: float
     name: testcolumnfloat
     order: 3`)
-    
-    err := os.WriteFile(configFile, yaml_txt, 0644)
+
+	err := os.WriteFile(configFile, yaml_txt, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// create table config
+	write_config()
+
+	sal := salmon.NewSalmon("localhost:27604")
+	sal.Init("/tmp/salmon.yaml")
+	sal.Start()
+    fmt.Println("Started salmon client")
+
+    myObj := map[string]interface{}{
+        "testcolumnint": (int32)(16),
+        "testcolumnstr": "tester",
+        "testcolumnfloat": (float64)(73.8),
+        "testcolumnbool": false,
+    }
+    err := sal.Insert("testtable", myObj)
     if err != nil {
         panic(err)
     }
-}
-
-func Example() {
     
-    // create table config
-    write_config()
-
-    sal := salmon.NewSalmon("localhost:27604")
-    sal.Init("/tmp/salmon.yaml")
-    sal.Start()
-
-    
+    fmt.Println("insert successfull, sleeping for 10s")
+    time.Sleep(time.Second * 10)
 }
